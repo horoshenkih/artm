c883ba299d099be19dc79e5744e8a32ebed43796
serkh <horoshenkih91@gmail.com>
2015-04-07 13:59:40 +0300

    EM and LDA algorithms

--- algorithms.py
+++ algorithms.py
@@ -1,29 +1,61 @@
 import numpy as np
 
-def em(collection, t, phi, theta, n_iter=10):
+def nmf(collection, t, phi, theta, n_iter=10, algorithm='em',params=None,verbose=False):
     w, d = collection.shape
     for iteration in range(n_iter):
+        if verbose:
+            print "Iteration", iteration
         n_wt = np.zeros((w,t))
         n_dt = np.zeros((d,t))
         product = phi * theta
+        if verbose:
+            print "All triples"
         for d_i in range(d):
             for w_i in range(w):
                 Z = product[w_i,d_i]
-                #print w_i, d_i, Z
                 for t_i in range(t):
-                    delta = collection[w_i, d_i]*phi[w_i,t_i]*theta[t_i,d_i]/Z
+                    delta = collection[w_i,d_i]*phi[w_i,t_i]*theta[t_i,d_i]/Z
                     n_wt[w_i,t_i] += delta
                     n_dt[d_i,t_i] += delta
-        for t_i in range(t):
-            n_t = n_wt[:,t_i].sum()
-            for w_i in range(w):
-                phi[w_i,t_i] = n_wt[w_i,t_i] / n_t
-        for d_i in range(d):
-            n_d = n_dt[d_i,:].sum()
-            for t_i in range(t):
-                theta[t_i,d_i] = n_dt[d_i,t_i] / n_d
-    return phi, theta
 
-def lda(collection, t, phi, theta):
-    # TODO
+        if verbose:
+            print "Fill Phi"
+        for t_i in range(t):
+            n_t = 0.
+            if algorithm == 'em':
+                for w_i in range(w):
+                    phi[w_i,t_i] = n_wt[w_i,t_i]
+                    n_t += n_wt[w_i,t_i]
+            elif algorithm == 'lda':
+                beta = params['beta']
+                for w_i in range(w):
+                    increment = n_wt[w_i,t_i] + beta[w_i]
+                    phi[w_i,t_i] = increment
+                    n_t += increment
+            else:
+                print "not implemented"
+                return
+            # normalize
+            for w_i in range(w):
+                phi[w_i,t_i] /= n_t
+        if verbose:
+            print "Fill Theta"
+        for d_i in range(d):
+            n_d = 0.
+            if algorithm == 'em':
+                for t_i in range(t):
+                    theta[t_i,d_i] = n_dt[d_i,t_i]
+                    n_d += n_dt[d_i,t_i]
+            elif algorithm == 'lda':
+                alpha = params['alpha']
+                for t_i in range(t):
+                    increment = n_dt[d_i,t_i] + alpha[t_i]
+                    theta[t_i,d_i] = increment
+                    n_d += increment
+            else:
+                print "not implemented"
+                return
+            # normalize
+            for t_i in range(t):
+                theta[t_i,d_i] /= n_d
     return phi, theta
--- tests.py
+++ tests.py
@@ -1,19 +1,45 @@
 from utils import *
+from algorithms import nmf
 import numpy as np
 
-w = 10
-d = 5
-t = 3
+w = 500
+d = 200
+t = 10
 
-'''
-alpha0 = 1
-beta0 = 0.1
-alpha = np.ones((1,t)).ravel() * alpha0
-beta = np.ones((1,w)).ravel() * beta0
+for alpha0 in [0.1, 0.5, 1., 2., 5., 10.]:
+    beta0 = 0.1  # const
+    alpha = np.ones((1,t)).ravel() * alpha0
+    beta = np.ones((1,w)).ravel() * beta0
 
-phi, theta, prod0, nd, prod = generate_all(w,d,t,alpha,beta,seed=31)
-print nd
-'''
-A = np.matrix([[1,1],[3,3]])
-B = np.matrix([[1,1],[3,3]])
-print dist(A,B)
+    phi, theta, prod0, nd, collection = generate_all(w,d,t,alpha,beta,seed=31)
+
+    seed=42
+    phi0 = generate_phi(w,t,beta,seed=seed)
+    theta0 = generate_theta(d,t,alpha,seed=seed)
+
+    '''
+    d_phi0 = dist(phi, phi0)
+    d_theta0 = dist(theta, theta0)
+    d_prod0 = dist (prod0, phi0 * theta0)
+    '''
+
+    algorithm = 'lda'
+    n_iter = 50
+    params = {
+        'alpha': alpha,
+        'beta': beta,
+    }
+
+    phi_em, theta_em = nmf(collection, t, phi0, theta0, algorithm=algorithm, n_iter=n_iter, params=params)
+
+    '''
+    print "Initial distances:"
+    print d_phi0
+    print d_theta0
+    print d_prod0
+    '''
+
+    print "Alpha0:", alpha0
+    print "D(phi, phi0):", dist(phi, phi_em)
+    print "D(theta, theta0)", dist(theta, theta_em)
+    print "D(prod, prod0)", dist (prod0, phi_em * theta_em)
