8d447aadf21b9ee885a5f83403a5692287d8dee5
serkh <horoshenkih91@gmail.com>
2015-04-08 18:13:40 +0300

    some fixes

--- algorithms.py
+++ algorithms.py
@@ -3,7 +3,7 @@ import numpy as np
 def nmf(collection, t, phi, theta, n_iter=10, algorithm='em',params=None,verbose=False):
     w, d = collection.shape
     losses_sliding_window = []
-    sliding_window_size = 3
+    sliding_window_size = 5
     for iteration in range(n_iter):
         if verbose:
             print "Iteration", iteration
@@ -64,27 +64,36 @@ def nmf(collection, t, phi, theta, n_iter=10, algorithm='em',params=None,verbose
         L = 0.
         for d_i in range(d):
             for w_i in range(w):
+                s = 0.
                 for t_i in range(t):
-                    L += collection[w_i, d_i] * np.log(phi[w_i, t_i]*theta[t_i, d_i])
+                    s += phi[w_i, t_i]*theta[t_i, d_i]
+                L += collection[w_i, d_i] * np.log(s)
         #L /= w * t * d
         loss_phi = 0.
-        beta = params['beta']
-        for w_i in range(w):
-            for t_i in range(t):
-                loss_phi += beta[w_i]*np.log(phi[w_i, t_i])
+        if algorithm == 'lda':
+            beta = params['beta']
+            for w_i in range(w):
+                for t_i in range(t):
+                    loss_phi += beta[w_i]*np.log(phi[w_i, t_i])
         #loss_phi /= w * t
         loss_theta = 0.
-        alpha = params['alpha']
-        for d_i in range(d):
-            for t_i in range(t):
-                loss_theta += alpha[t_i]*np.log(theta[t_i, d_i])
+        if algorithm == 'lda':
+            alpha = params['alpha']
+            for d_i in range(d):
+                for t_i in range(t):
+                    loss_theta += alpha[t_i]*np.log(theta[t_i, d_i])
         #loss_theta /= d * t
         if verbose:
             print "Likelihood:", L
-            print "Loss phi:", loss_phi
-            print "Loss theta:", loss_theta
+            if algorithm == 'lda':
+                print "Loss phi:", loss_phi
+                print "Loss theta:", loss_theta
         # collect sliding window
-        current_loss = L + loss_theta + loss_phi
+        current_loss = 0.
+        if algorithm == 'em':
+            current_loss = L
+        elif algorithm == 'lda':
+            current_loss = L + loss_theta + loss_phi
         if iteration < 2 * sliding_window_size:
             losses_sliding_window.append(current_loss)
         else:
@@ -97,9 +106,11 @@ def nmf(collection, t, phi, theta, n_iter=10, algorithm='em',params=None,verbose
                 print "early loss:", early_loss
                 print "later loss:", later_loss
                 print "/ early stopping data"
-            if later_loss < early_loss:
+            if later_loss < early_loss: # and iteration >= 20:
+                print "Took iterations:", iteration
                 return phi, theta
             # add new loss
             losses_sliding_window.pop(0)
             losses_sliding_window.append(current_loss)
+    print "Took iterations:", iteration
     return phi, theta
--- tests.py
+++ tests.py
@@ -11,35 +11,23 @@ for alpha0 in [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1., 2.]:
     alpha = np.ones((1,t)).ravel() * alpha0
     beta = np.ones((1,w)).ravel() * beta0
 
-    phi, theta, prod0, nd, collection = generate_all(w,d,t,alpha,beta,seed=30)
+    phi0, theta0, prod0, nd, collection = generate_all(w,d,t,alpha,beta,seed=30)
 
     seed=40
-    phi0 = generate_phi(w,t,beta,seed=seed)
-    theta0 = generate_theta(d,t,alpha,seed=seed)
+    phi1 = generate_phi(w,t,beta,seed=seed)
+    theta1 = generate_theta(d,t,alpha,seed=seed)
 
-    '''
-    d_phi0 = dist(phi, phi0)
-    d_theta0 = dist(theta, theta0)
-    d_prod0 = dist (prod0, phi0 * theta0)
-    '''
-
-    algorithm = 'lda'
     n_iter = 100
     params = {
         'alpha': alpha,
         'beta': beta,
     }
 
-    phi_em, theta_em = nmf(collection, t, phi0, theta0, algorithm=algorithm, n_iter=n_iter, params=params, verbose=False)
-
-    '''
-    print "Initial distances:"
-    print d_phi0
-    print d_theta0
-    print d_prod0
-    '''
-
     print "Alpha0:", alpha0
-    print "D(phi, phi0):", dist(phi, phi_em)
-    print "D(theta, theta0)", dist(theta, theta_em)
-    print "D(prod, prod0)", dist (prod0, phi_em * theta_em)
+    for algorithm in ['lda']:
+        phi, theta = nmf(collection, t, phi1, theta1, algorithm=algorithm, n_iter=n_iter, params=params, verbose=False)
+
+        print "Algorithm:", algorithm
+        print "D(phi, phi0):", dist(phi0, phi)
+        print "D(theta, theta0)", dist(theta0, theta)
+        print "D(prod, prod0)", dist (prod0, phi * theta)
