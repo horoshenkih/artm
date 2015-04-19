9bdbd0e5a53c7d66fa1fffe9b86ba5df1102b20b
khoroshenkikh <horoshenkih91@gmail.com>
2015-04-19 13:28:02 +0300

    matrix generator

--- matlab/frey.m
+++ matlab/frey.m
@@ -1,7 +1,18 @@
-T = 3; W = T; D = T; F = eye(T);
-Phi =   [0.2755, 0.9333, 0.6804; 0.9871, 0.3484, 0.3554; 0.7950, 0.2091, 0.4384]
-Theta = [0.3380, 0.6988, 0.6354; 0.7343, 0.0899, 0.5253; 0.9855, 0.4707, 0.1413]
-for i=1:6
+% problem parameters
+T = 10; W = T; D = T; F = eye(T);
+
+% prepare RNG
+rand('seed',42)
+
+% prepare matrices
+Phi = rand(T); Phi = Phi ./ sum(Phi);
+Theta = ones(T) ./ T;
+
+printf("Initial matrices:\n")
+Phi
+Theta
+% run algorithm (Frey's code)
+for i=1:10
     Z = F ./ (Phi * Theta); Z(F==0) = 0; % this line is correct but really slow
     Phi_tmp = Phi .* (Z * Theta');
     Theta_tmp = Theta .* (Phi' * Z);    
@@ -9,8 +20,13 @@ for i=1:6
     Theta = Theta_tmp ./ repmat(sum(Theta_tmp), T, 1);
 end
 
-% rounding issues in Octave
-round(Phi*100) ./ 100
-round(Theta*100) ./ 100
+% plot results (rounding issues in Octave)
+Phi = round(Phi*100) ./ 100;
+Theta = round(Theta*100) ./ 100;
 
-round(Phi * Theta * 100) ./ 100
+product = round(Phi * Theta * 100) ./ 100;
+
+printf("Resulting matrices:\n")
+Phi
+Theta
+product
