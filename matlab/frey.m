c5719553216233b90b7b62ba2e37c5873e417ce8
khoroshenkikh <horoshenkih91@gmail.com>
2015-04-19 13:58:06 +0300

    random initialization and error calculation

--- matlab/frey.m
+++ matlab/frey.m
@@ -1,32 +1,41 @@
 % problem parameters
 T = 10; W = T; D = T; F = eye(T);
 
-% prepare RNG
-rand('seed',42)
+n_runs = 100
+errors = [];
+for run=1:n_runs
+    % prepare RNG
+    rand('seed',run)
 
-% prepare matrices
-Phi = rand(T); Phi = Phi ./ sum(Phi);
-Theta = ones(T) ./ T;
+    % prepare matrices
+    Phi = rand(T); Phi = Phi ./ sum(Phi);
+    Theta = rand(T); Phi = Phi ./ sum(Phi);
+    %Theta = ones(T) ./ T;
 
-printf("Initial matrices:\n")
-Phi
-Theta
-% run algorithm (Frey's code)
-for i=1:10
-    Z = F ./ (Phi * Theta); Z(F==0) = 0; % this line is correct but really slow
-    Phi_tmp = Phi .* (Z * Theta');
-    Theta_tmp = Theta .* (Phi' * Z);    
-    Phi = Phi_tmp ./ repmat(sum(Phi_tmp), W, 1);
-    Theta = Theta_tmp ./ repmat(sum(Theta_tmp), T, 1);
+    %printf('Initial matrices:\n')
+    %Phi
+    %Theta
+    % run algorithm (Frey's code)
+    for i=1:10
+        Z = F ./ (Phi * Theta); Z(F==0) = 0; % this line is correct but really slow
+        Phi_tmp = Phi .* (Z * Theta');
+        Theta_tmp = Theta .* (Phi' * Z);    
+        Phi = Phi_tmp ./ repmat(sum(Phi_tmp), W, 1);
+        Theta = Theta_tmp ./ repmat(sum(Theta_tmp), T, 1);
+    end
+
+    % plot results (rounding issues in Octave)
+    Phi = round(Phi*100) ./ 100;
+    Theta = round(Theta*100) ./ 100;
+
+    product = round(Phi * Theta * 100) ./ 100;
+
+    %printf('Resulting matrices:\n')
+    %Phi
+    %Theta
+    %printf('Error:\n')
+    %product - F
+    errors = [errors norm(product - F, 1)];
 end
-
-% plot results (rounding issues in Octave)
-Phi = round(Phi*100) ./ 100;
-Theta = round(Theta*100) ./ 100;
-
-product = round(Phi * Theta * 100) ./ 100;
-
-printf("Resulting matrices:\n")
-Phi
-Theta
-product
+idx = find(errors > 0);
+printf('Correct decomposition in %f %% cases\n', 100 * (1 - columns(idx) / n_runs));
